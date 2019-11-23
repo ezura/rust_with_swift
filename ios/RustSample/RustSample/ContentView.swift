f227f2d7f9a136767ace936e4e476f9d04738f95
ezura <yuka.ezura@gmail.com>
2019-11-23 16:03:30 +0900

    Create iOS app project

---
+++ ios/RustSample/RustSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RustSample
+//
+//  Created by yuka ezura on 2019/11/23.
+//  Copyright © 2019 繪面友香. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
